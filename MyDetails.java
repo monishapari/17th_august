5deff0bbf0c95902cc964f40898cbd8f499eb64e
monishapari <89031449+monishapari@users.noreply.github.com>
2021-08-17 22:18:06 +0530

    Add files via upload

---
+++ MyDetails.java
@@ -0,0 +1,17 @@
+public class MyDetails {
+	
+	void personalInfo()
+	{
+		System.out.println("Personal Details:");
+		System.out.println("Name: Monisha P");
+		System.out.println("Date of Birth: 16.10.1998");
+		System.out.println("Qualification: M.Tech Integrated Software");
+	}
+
+	public static void main(String[] args) {
+		MyDetails obj = new MyDetails();
+		obj.personalInfo();
+
+	}
+
+}
